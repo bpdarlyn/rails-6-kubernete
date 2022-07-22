d1be96656b7b0b7b4d4e4d76edc150434f2fb9fb
Darlyn <bpdarlyn@gmail.com>
2022-07-21 22:41:47 -0400

    adding entrypoint

---
+++ docker/production/entrypoint.sh
@@ -0,0 +1,9 @@
+# bundle install
+# yarn install --frozen-lockfile --non-interactive --production
+
+# bundle exec rails assets:precompile
+# bundle exec rails db:migrate
+
+
+# cmd="$@"
+# exec $cmd
